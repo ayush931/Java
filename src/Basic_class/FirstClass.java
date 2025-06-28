a6a6db78e4270ec3c8d2efe50e3b5b2200da2ae1
ayush931 <ayushkumar9315983@gmail.com>
2025-06-28 22:40:35 +0530

    basic of java

---
+++ src/Basic_class/FirstClass.java
@@ -0,0 +1,11 @@
+package Basic_class;
+
+// Everything in java is written in class
+// class:- a user defined blueprint consisting of attributes and methods (functions)
+// object:- instance of the class
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
